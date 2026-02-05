f51f8b74035a9b1357b5fbb3194fed4e7d1e0aba
smallsm1245-cyber <smallsm1245@gmail.com>
2026-02-05 16:14:24 +0900

    config.js 만들기

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js로 복사하여 사용하세요.
+const CONFIG = {
+    SUPABASE_URL: "https://ztheweqgzmtmazvyuzpa.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0aGV3ZXFnem10bWF6dnl1enBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMjQ0NTIsImV4cCI6MjA4NTgwMDQ1Mn0.CJaWqWJzozfcBafsGykSk0Z_eOXUIVAkMhTw3WZr8Ts",
+    ADMIN_EMAIL: "smallsm@naver.com" // 관리자 화이트리스트 이메일
+};
