db76cb8b88fd86c3123bf08e332ce47d3fe3f03f
smallsm1245-cyber <smallsm1245@gmail.com>
2026-02-05 16:22:52 +0900

    config.js 업데이트

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const CONFIG = {
+    // 마지막에 슬래시(/)가 없는지, 오타가 없는지 확인된 주소입니다.
     SUPABASE_URL: "https://ztheweqgzmtmazvyuzpa.supabase.co",
+    // ANON KEY 값입니다.
     SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0aGV3ZXFnem10bWF6dnl1enBhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMjQ0NTIsImV4cCI6MjA4NTgwMDQ1Mn0.CJaWqWJzozfcBafsGykSk0Z_eOXUIVAkMhTw3WZr8Ts",
     ADMIN_EMAIL: "smallsm@naver.com"
 };
