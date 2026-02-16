dabbda4d68d00b82c2ecda543db7f7c521171f2f
smallsm1245-cyber <smallsm1245@gmail.com>
2026-02-16 22:49:55 +0900

    Delete database-update-v3.sql

--- database-update-v3.sql
+++
@@ -1,9 +0,0 @@
--- Add parent_id column to categories table for 2-depth hierarchy
-ALTER TABLE "public"."categories" 
-ADD COLUMN "parent_id" uuid REFERENCES "public"."categories"("id") ON DELETE SET NULL;
-
--- Add comment
-COMMENT ON COLUMN "public"."categories"."parent_id" IS 'Parent category ID for nested structure (NULL = Top level)';
-
--- Policy update (if needed, usually existing policies cover updates)
--- Assuming existing RLS allows update if user is authenticated/admin.
